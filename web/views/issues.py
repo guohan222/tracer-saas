abfddbd4c37986f4f73cc8add4b89521a9c6cb55
guohan222 <guohan0826@gmail.com>
2026-04-07 22:12:11 +0800

    问题更新：fk处理

--- web/views/issues.py
+++ web/views/issues.py
@@ -1,12 +1,10 @@
-
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from web import models
-from web.forms.issues import IssuesModelForm,IssuesReplyModelForm
+from web.forms.issues import IssuesModelForm, IssuesReplyModelForm
 from utils.pagination import Pagination
-
 
 
 # 展示&新建问题
@@ -27,7 +25,7 @@ def issues(request, proj_id):
         content = {
             'issues_object_list': issues_obj_list,
             'page_html': page_obj.page_html(),
-            'form':form
+            'form': form
         }
         return render(request, 'issues.html', content)
 
@@ -43,15 +41,33 @@ def issues(request, proj_id):
 
 # 编辑问题
 @csrf_exempt
-def issues_detail(request,proj_id,issues_id):
+def issues_detail(request, proj_id, issues_id):
     issues_obj = models.Issues.objects.filter(id=issues_id).first()
+
+    def create_reply_record(content):
+        # 创建一条操作评论
+        reply_obj = models.IssuesReply.objects.create(
+            reply_type=1,
+            issues_id=issues_id,
+            creator_id=request.tracer.user.id,
+            content=content
+        )
+        # 给前端返回该评论进行挂载时需要的东西
+        data_dict = {
+            'id': reply_obj.id,
+            'reply_type': reply_obj.get_reply_type_display(),
+            'creator_name': reply_obj.creator.name,
+            'content': reply_obj.content,
+            'create_datetime': reply_obj.create_datetime,
+            'parent_id': reply_obj.parent.id if reply_obj.parent else ''
+        }
+        return data_dict
 
     # 展示原有数据
     if request.method == "GET":
-        form = IssuesModelForm(request,instance=issues_obj)
-        return render(request,'issues_detail.html',{'form':form,'issues_obj':issues_obj})
+        form = IssuesModelForm(request, instance=issues_obj)
+        return render(request, 'issues_detail.html', {'form': form, 'issues_obj': issues_obj})
 
-    # 更新数据
     """
     普通文本字段：subject、desc、start_date、end_date
     choices字段：status、priority、mode
@@ -60,84 +76,97 @@ def issues_detail(request,proj_id,issues_id):
     
     前端传来的数据格式：{field:xxx,value:xxx}
     """
+    # 更新数据
     field = request.POST.get('field')
     value = request.POST.get('value')
     # 拿到该字段对象
     field_obj = models.Issues._meta.get_field(field)
 
     # 1. 普通文本字段
-    if str(field) in ['subject','desc','start_date','end_date']:
+    if field in ['subject', 'desc', 'start_date', 'end_date']:
         # 判断值是否为空，为空则检查：是否允许为空
         if not value:
             if not field_obj.null:
-                return JsonResponse({'status':False,'errors':'值不能为空！'})
+                return JsonResponse({'status': False, 'errors': '值不能为空！'})
             # 否则允许为空，进行更新保存
-            setattr(issues_obj,field,None)
-            issues_obj.save(update_fields=[field,])
-            record_content = f'{field_obj.verbose_name}更新为:{value}'
-        else:
-            setattr(issues_obj, field, value)
+            setattr(issues_obj, field, None)
             issues_obj.save(update_fields=[field, ])
             record_content = f'{field_obj.verbose_name}更新为:{value}'
+        else:
+            print(value)
+            setattr(issues_obj, field, value)
+            issues_obj.save(update_fields=[field, ])
+            record_content = f'更新了问题描述' if field == 'desc' else f'{field_obj.verbose_name}更新为:{value}'
 
-        # 创建一条操作评论
-        reply_obj = models.IssuesReply.objects.create(
-            reply_type = 1,
-            issues_id = issues_id,
-            creator_id = request.tracer.user.id,
-            content = record_content
-        )
-        # 给前端返回该评论进行挂载时需要的东西
-        data = {
-            'id': reply_obj.id,
-            'reply_type': reply_obj.get_reply_type_display(),
-            'creator_name': reply_obj.creator.name,
-            'content': reply_obj.content,
-            'create_datetime': reply_obj.create_datetime,
-            'parent_id': reply_obj.parent.id if reply_obj.parent else ''
-        }
-        return JsonResponse({'status':True,'reply_obj':data})
+        data = create_reply_record(record_content)
+
+        return JsonResponse({'status': True, 'reply_obj': data})
+
+    # 2. FK字段处理(FK字段时value为关联的对象的id)     issues_type、module、assign、parent
+    if field in ['issues_type', 'module', 'parent', 'assign']:
+        # 判断值是否为空，为空则检查：是否允许为空
+        if not value:
+            if not field_obj.null:
+                return JsonResponse({'status': False, 'errors': '值不能为空！'})
+            setattr(issues_obj, field, None)
+            issues_obj.save(update_fields=[field])
+            record_content = f'{field_obj.verbose_name}更新为:{value}'
+
+        else:  # 不为空则检测FK字段有没有其他要求，比如assign只能为该项目中的参与者或者创建者
+
+            # 如果字段为assign,则要判断能否根据这个value找到属于该项目的人，如果不能则表明操作者通过手段，试图派给不属于该项目中的人
+            if field == 'assign':
+                # 检测是否派给了创建者
+                if value == str(request.tracer.user.id):
+                    instance = request.tracer.user
+                else:  # 否则是派给了参与者，则需检测能否根据这个value(此时为id)在这个项目中找到具体的人
+                    participants_obj = models.Participants.objects.filter(project_id=proj_id, user_id=value).first()
+                    if participants_obj:
+                        instance = participants_obj.user
+                    else:  # 用户通过手段，试图派给不属于该项目中的人
+                        return JsonResponse({'status': False, 'errors': '选择的值不存在'})
+
+                # 上面通过则代表操作者正常执行，assign字段待更新的值的合法的
+                setattr(issues_obj, field, instance)
+                issues_obj.save(update_fields=[field])
+                record_content = f'{field_obj.verbose_name}更新为:{str(instance)}'
+
+            else:  # 检测其他FK字段根据value查找，是否真实存在且合法
+                """
+                    外键字段.remote_field——》找到这个外键字段关联哪张表
+                    外键字段.remote_field.model——》这个外键字段关联的那张表的model类
+                """
+                instance = field_obj.remote_field.model.objects.filter(id=value, project_id=proj_id).first()
+                if not instance:
+                    return JsonResponse({'status': False, 'errors': '选择的值不存在'})
+                setattr(issues_obj, field, instance)
+                issues_obj.save(update_fields=[field])
+                record_content = f'{field_obj.verbose_name}更新为:{str(instance)}'
+
+        data = create_reply_record(record_content)
+        return JsonResponse({'status': True, 'reply_obj': data})
 
     return HttpResponse('sb')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # 展示&新建评论
 @csrf_exempt
-def issues_record(request,proj_id,issues_id):
+def issues_record(request, proj_id, issues_id):
     if request.method == "GET":
         # 按时间正序排,防止子评论早出现找不到父评论
         reply_objs = models.IssuesReply.objects.filter(issues_id=issues_id).order_by('create_datetime')
         reply_obj_list = []
         for row in reply_objs:
             data = {
-                'id':row.id,
-                'reply_type':row.get_reply_type_display(),
-                'creator_name':row.creator.name,
-                'content':row.content,
-                'create_datetime':row.create_datetime,
-                'parent_id':row.parent.id if row.parent else ''
+                'id': row.id,
+                'reply_type': row.get_reply_type_display(),
+                'creator_name': row.creator.name,
+                'content': row.content,
+                'create_datetime': row.create_datetime,
+                'parent_id': row.parent.id if row.parent else ''
             }
             reply_obj_list.append(data)
-        return JsonResponse({'status':True,'reply_obj_list':reply_obj_list})
+        return JsonResponse({'status': True, 'reply_obj_list': reply_obj_list})
 
     # 新建评论
     form = IssuesReplyModelForm(data=request.POST)
@@ -154,8 +183,5 @@ def issues_record(request,proj_id,issues_id):
             'create_datetime': reply_obj.create_datetime,
             'parent_id': reply_obj.parent.id if reply_obj.parent else ''
         }
-        return JsonResponse({'status':True,'reply_obj':data})
-    return JsonResponse({'status':False,'errors':form.errors.get_json_data()})
-
-
-
+        return JsonResponse({'status': True, 'reply_obj': data})
+    return JsonResponse({'status': False, 'errors': form.errors.get_json_data()})
